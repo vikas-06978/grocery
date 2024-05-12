28a385933a7228ed924da8566594c54cab910aea
vikas-06978 <vikas9267981@gmail.com>
2024-05-12 16:23:15 +0530

    create admin dashboard and admin required

--- routes.py
+++ routes.py
@@ -78,13 +78,32 @@ def auth_required(func):
             return redirect(url_for('login'))
     return inner
 
+# decorator for admin_required
+
+def admin_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('Please login to continue')
+            return redirect(url_for('login'))
+        user = User.query.get(session['user_id'])
+        if not user.is_admin:
+            flash('You are not authorized to access this page')
+            return redirect(url_for('index'))
+        return func(*args, **kwargs)
+    return inner
+
 
 # index route
 @app.route('/')
 @auth_required
 def index():
-    # user_id in session
+    user = User.query.get(session['user_id'])
+    if user.is_admin:
+        return redirect(url_for('admin'))
     return render_template('index.html')
+    
+    # user_id in session
     
     # if 'user_id' in session:
     #     return render_template('index.html')
@@ -146,6 +165,31 @@ def profile_post():
 def logout():
     session.pop('user_id')
     return redirect(url_for('login'))
+
+    # ----- admin pages
+
+@app.route('/admin')
+@admin_required
+def admin():
+    return render_template('admin.html')
     
+@app.route('/category/add')
+@admin_required
+def add_category():
+    return 'add category'
+
+@app.route('/category/<int:id>/')
+@admin_required
+def show_category(id):
+    return 'show category'    
     
+@app.route('/category/<int:id>/edit')
+@admin_required
+def edit_category(id):
+    return 'edit category'    
+    
+@app.route('/category/<int:id>/delete')
+@admin_required
+def delete_category(id):
+    return 'delete category'    
     
