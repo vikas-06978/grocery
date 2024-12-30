bdf6bd8c6d121f516b8ce6382bd2d42a3985cff6
Vikas Rathore <124425681+vikas-06978@users.noreply.github.com>
2024-12-30 19:18:57 +0530

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2024 Vikas-06978
+# Licensed under the MIT License
+# Unauthorized copying of this file, via any medium, is strictly prohibited
+# Written by Vikas
+
+
 from dotenv import load_dotenv
 import os
 from app import app
