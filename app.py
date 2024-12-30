37b099a34a3fe81033711d23e272b571f84ed331
Vikas Rathore <124425681+vikas-06978@users.noreply.github.com>
2024-12-30 19:19:13 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2024 Vikas-06978
+# Licensed under the MIT License
+# Unauthorized copying of this file, via any medium, is strictly prohibited
+# Written by Vikas
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
